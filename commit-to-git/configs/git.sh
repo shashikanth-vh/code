7cf486e4208109dbec70a8d06f8017043f44f71b
shashikanth-vh <shashikanth.vh@gmail.com>
2022-11-25 11:00:10 +0530

    fixed

--- commit-to-git/configs/git.sh
+++ commit-to-git/configs/git.sh
@@ -9,4 +9,5 @@ ${GIT} commit -m "$message"
 
 #gitPush=$(${GIT} push -vvv git@github.com:shashikanth-vh/helm-charts.git master 2>&1)
 #echo "$gitPush"
-git push origin main
+#git push origin main
+git push https://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@github.com/shashikanth-vh/helm-charts.git
